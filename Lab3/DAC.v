766b36bc0e86481d96e1b4dcbfada61abf32c14e
Styxx <Styxx_N_Stones@live.com>
2015-02-25 20:55:41 -0800

    Update DAC.v

--- Lab3/DAC.v
+++ Lab3/DAC.v
@@ -1,3 +1,4 @@
+// Code your design here
 /***************************************************************/
 /*  DAC verilog												   */
 /*  Sahil Bissessur, Vincent "Styxx" Chang, Enrique Gutierrez  */
@@ -14,16 +15,19 @@ module DAC(add, clk, strobe, WR, Dout);
 	output reg WR;			//Equiv of ready?
 	output reg [7:0] Dout;
 
-	reg [7:0] Din = 8'b10000000;
-	reg [7:0] preDin = 8'b01111111;
-
+	//reg [7:0] Din = 8'b10000000;
+	//reg [7:0] preDin = 8'b01111111;
+	reg [7:0] Din = 8'b00000000;
+	reg up = 1;
+	reg [31:0] counter = 0;
+	
 	
 	parameter starting =	2'b00;
 	parameter pre_latch =	2'b01;
-	parameter latch =		2'b10;
+	parameter latch =	2'b10;
 	parameter post_latch =	2'b11;
 
-	reg [1:0] state = starting;
+	reg [1:0] state = pre_latch;
 	
 	// Process: Address valid --> WR low --> Data valid
 	
@@ -35,12 +39,13 @@ module DAC(add, clk, strobe, WR, Dout);
 			starting:
 				begin
 					// Output
-					Dout <= 7'bz;
+					//Dout <= 7'bz;
 					
 					// Next State
-					if((add[11:0] == 12'h10d) && (~strobe)) begin
+					//if((add[11:0] == 12'h10d) && (~strobe)) begin
 						state <= pre_latch;
-					end
+						WR <= 1'b0;
+					//end
 						
 				end
 			
@@ -49,58 +54,25 @@ module DAC(add, clk, strobe, WR, Dout);
 			pre_latch:
 				begin
 					// Output
-					WR = 1'b0;
-					
-					// Change Din to next value
-					if (Din == 8'b00000000) begin			// Can only go up
-						Din <= 8'b00000001;
-						preDin <= 8'b00000000;
+					if (counter == 200) begin					// Smaller counter = higher frequency
+						counter <= 0;
+						if (up == 1) begin
+							Din <= Din + 1;
+							if (Din == 8'b11111110) begin
+								up <= 0;
+							end
+						end
+						else begin
+							Din <= Din - 1;
+							if (Din == 8'b00000001)	begin
+								up <= 1;
+							end
+						end
+						
 					end
-					else if (Din == 8'b00000001) begin
-						if (preDin == 8'b00000000) begin	// Going up
-							Din <= 8'b01111111;
-							preDin = 8'b00000001;
-						end
-						else begin		// Going down
-							Din <= 8'b00000000;
-							preDin = 8'b00000001;
-						end
+					else begin
+						counter <= counter + 1;
 					end
-					else if (Din = 8'b01111111) begin
-						if (preDin == 8'b00000001) begin	// Going up
-							Din <= 8'b10000000;
-							preDin <= 8'b01111111;
-						end
-						else begin		// Going down
-							Din <= 8'b00000001;
-							preDin <= 8'b01111111;
-						end
-					end
-					else if (Din = 8'b10000000) begin
-						if (preDin == 8'b01111111) begin	// Going up
-							Din <= 8'b10000001;
-							preDin <= 8'b10000000;
-						end
-						else begin		// Going down
-							Din <= 8'b01111111;
-							preDin <= 8'b10000000;
-						end
-					end
-					else if (Din = 8'b10000001) begin
-						if (preDin == 8'b10000000) begin	// Going up
-							Din <= 8'b11111111;
-							preDin <= 8'b10000001;
-						end
-						else begin		//Going down
-							Din <= 8'b10000000;
-							preDin <= 8'b10000001;
-						end
-					end
-					else begin	//Din = 8'b11111111			Can only go down
-						Din = 8'b10000001;
-						preDin = 8'b11111111;
-					end
-					
 					
 					// Next State
 					state <= latch;
@@ -126,7 +98,7 @@ module DAC(add, clk, strobe, WR, Dout);
 					WR <= 1'b1;
 					
 					// Next State
-					state <= starting;
+					state <= pre_latch;
 				
 				end
 				
